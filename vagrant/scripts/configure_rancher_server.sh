4195d06ea9d27d207f9b4600072afe7f66d59619
David Noland <dnoland@rancher.com>
2018-09-24 21:56:31 -0700

    Parameterized Kubernetes version and switched from v1.10.1 to v1.11.2.

--- vagrant/scripts/configure_rancher_server.sh
+++ vagrant/scripts/configure_rancher_server.sh
@@ -3,6 +3,7 @@
 rancher_server_ip="172.22.101.101"
 default_password=${1:-password}
 rancher_server_version=${2:-stable}
+kubernetes_version=${3:-v1.10.1-rancher1}
 registry_prefix="rancher"
 curl_prefix="appropriate"
 
@@ -64,7 +65,7 @@ CLUSTERRESPONSE=$(docker run --net host \
     --rm \
     $curl_prefix/curl \
      -s 'https://127.0.0.1/v3/cluster' -H 'content-type: application/json' -H "Authorization: Bearer $APITOKEN" \
-     --data-binary '{"type":"cluster","rancherKubernetesEngineConfig":{"ignoreDockerVersion":false,"sshAgentAuth":false,"type":"rancherKubernetesEngineConfig","kubernetesVersion":"v1.10.1-rancher1","authentication":{"type":"authnConfig","strategy":"x509"},"network":{"type":"networkConfig","plugin":"flannel","flannelNetworkProvider":{"iface":"eth1"},"calicoNetworkProvider":null},"ingress":{"type":"ingressConfig","provider":"nginx"},"services":{"type":"rkeConfigServices","kubeApi":{"podSecurityPolicy":false,"type":"kubeAPIService"},"etcd":{"type":"etcdService","extraArgs":{"heartbeat-interval":500,"election-timeout":5000}}}},"name":"quickstart"}' --insecure)
+     --data-binary "{\"type\":\"cluster\",\"rancherKubernetesEngineConfig\":{\"ignoreDockerVersion\":false,\"sshAgentAuth\":false,\"type\":\"rancherKubernetesEngineConfig\",\"kubernetesVersion\":\"$kubernetes_version\",\"authentication\":{\"type\":\"authnConfig\",\"strategy\":\"x509\"},\"network\":{\"type\":\"networkConfig\",\"plugin\":\"flannel\",\"flannelNetworkProvider\":{\"iface\":\"eth1\"},\"calicoNetworkProvider\":null},\"ingress\":{\"type\":\"ingressConfig\",\"provider\":\"nginx\"},\"services\":{\"type\":\"rkeConfigServices\",\"kubeApi\":{\"podSecurityPolicy\":false,\"type\":\"kubeAPIService\"},\"etcd\":{\"type\":\"etcdService\",\"extraArgs\":{\"heartbeat-interval\":500,\"election-timeout\":5000}}}},\"name\":\"quickstart\"}" --insecure)
 
 # Extract clusterid to use for generating the docker run command
 CLUSTERID=`echo $CLUSTERRESPONSE | jq -r .id`
