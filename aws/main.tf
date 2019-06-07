c2e2b5c7319a57ef6406cfb90a117557e73b4add
zacanger <zac@zacanger.com>
2019-06-07 17:11:45 -0600

    Updates for current TF

--- aws/main.tf
+++ aws/main.tf
@@ -1,8 +1,8 @@
 # Configure the Amazon AWS Provider
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.region}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
 }
 
 variable "aws_access_key" {
@@ -121,24 +121,24 @@ data "template_cloudinit_config" "rancherserver-cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.userdata_server.rendered}"
+    content      = data.template_file.userdata_server.rendered
   }
 }
 
 resource "aws_instance" "rancherserver" {
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "${var.type}"
-  key_name        = "${var.ssh_key_name}"
-  security_groups = ["${aws_security_group.rancher_sg_allowall.name}"]
-  user_data       = "${data.template_cloudinit_config.rancherserver-cloudinit.rendered}"
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.rancher_sg_allowall.name]
+  user_data       = data.template_cloudinit_config.rancherserver-cloudinit.rendered
 
-  tags {
+  tags = {
     Name = "${var.prefix}-rancherserver"
   }
 }
 
 data "template_cloudinit_config" "rancheragent-all-cloudinit" {
-  count = "${var.count_agent_all_nodes}"
+  count = var.count_agent_all_nodes
 
   part {
     content_type = "text/cloud-config"
@@ -147,25 +147,25 @@ data "template_cloudinit_config" "rancheragent-all-cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.userdata_agent.rendered}"
+    content      = data.template_file.userdata_agent.rendered
   }
 }
 
 resource "aws_instance" "rancheragent-all" {
-  count           = "${var.count_agent_all_nodes}"
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "${var.type}"
-  key_name        = "${var.ssh_key_name}"
-  security_groups = ["${aws_security_group.rancher_sg_allowall.name}"]
-  user_data       = "${data.template_cloudinit_config.rancheragent-all-cloudinit.*.rendered[count.index]}"
+  count           = var.count_agent_all_nodes
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.rancher_sg_allowall.name]
+  user_data       = data.template_cloudinit_config.rancheragent-all-cloudinit[count.index].rendered
 
-  tags {
+  tags = {
     Name = "${var.prefix}-rancheragent-${count.index}-all"
   }
 }
 
 data "template_cloudinit_config" "rancheragent-etcd-cloudinit" {
-  count = "${var.count_agent_etcd_nodes}"
+  count = var.count_agent_etcd_nodes
 
   part {
     content_type = "text/cloud-config"
@@ -174,25 +174,25 @@ data "template_cloudinit_config" "rancheragent-etcd-cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.userdata_agent.rendered}"
+    content      = data.template_file.userdata_agent.rendered
   }
 }
 
 resource "aws_instance" "rancheragent-etcd" {
-  count           = "${var.count_agent_etcd_nodes}"
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "${var.type}"
-  key_name        = "${var.ssh_key_name}"
-  security_groups = ["${aws_security_group.rancher_sg_allowall.name}"]
-  user_data       = "${data.template_cloudinit_config.rancheragent-etcd-cloudinit.*.rendered[count.index]}"
+  count           = var.count_agent_etcd_nodes
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.rancher_sg_allowall.name]
+  user_data       = data.template_cloudinit_config.rancheragent-etcd-cloudinit[count.index].rendered
 
-  tags {
+  tags = {
     Name = "${var.prefix}-rancheragent-${count.index}-etcd"
   }
 }
 
 data "template_cloudinit_config" "rancheragent-controlplane-cloudinit" {
-  count = "${var.count_agent_controlplane_nodes}"
+  count = var.count_agent_controlplane_nodes
 
   part {
     content_type = "text/cloud-config"
@@ -201,25 +201,25 @@ data "template_cloudinit_config" "rancheragent-controlplane-cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.userdata_agent.rendered}"
+    content      = data.template_file.userdata_agent.rendered
   }
 }
 
 resource "aws_instance" "rancheragent-controlplane" {
-  count           = "${var.count_agent_controlplane_nodes}"
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "${var.type}"
-  key_name        = "${var.ssh_key_name}"
-  security_groups = ["${aws_security_group.rancher_sg_allowall.name}"]
-  user_data     = "${data.template_cloudinit_config.rancheragent-controlplane-cloudinit.*.rendered[count.index]}"
+  count           = var.count_agent_controlplane_nodes
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.rancher_sg_allowall.name]
+  user_data       = data.template_cloudinit_config.rancheragent-controlplane-cloudinit[count.index].rendered
 
-  tags {
+  tags = {
     Name = "${var.prefix}-rancheragent-${count.index}-controlplane"
   }
 }
 
 data "template_cloudinit_config" "rancheragent-worker-cloudinit" {
-  count = "${var.count_agent_worker_nodes}"
+  count = var.count_agent_worker_nodes
 
   part {
     content_type = "text/cloud-config"
@@ -228,47 +228,47 @@ data "template_cloudinit_config" "rancheragent-worker-cloudinit" {
 
   part {
     content_type = "text/x-shellscript"
-    content      = "${data.template_file.userdata_agent.rendered}"
+    content      = data.template_file.userdata_agent.rendered
   }
 }
 
 resource "aws_instance" "rancheragent-worker" {
-  count           = "${var.count_agent_worker_nodes}"
-  ami             = "${data.aws_ami.ubuntu.id}"
-  instance_type   = "${var.type}"
-  key_name        = "${var.ssh_key_name}"
-  security_groups = ["${aws_security_group.rancher_sg_allowall.name}"]
-  user_data       = "${data.template_cloudinit_config.rancheragent-worker-cloudinit.*.rendered[count.index]}"
+  count           = var.count_agent_worker_nodes
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.rancher_sg_allowall.name]
+  user_data       = data.template_cloudinit_config.rancheragent-worker-cloudinit[count.index].rendered
 
-  tags {
+  tags = {
     Name = "${var.prefix}-rancheragent-${count.index}-worker"
   }
 }
 
 data "template_file" "userdata_server" {
-  template = "${file("files/userdata_server")}"
+  template = file("files/userdata_server")
 
-  vars {
-    admin_password        = "${var.admin_password}"
-    cluster_name          = "${var.cluster_name}"
-    docker_version_server = "${var.docker_version_server}"
-    rancher_version       = "${var.rancher_version}"
+  vars = {
+    admin_password        = var.admin_password
+    cluster_name          = var.cluster_name
+    docker_version_server = var.docker_version_server
+    rancher_version       = var.rancher_version
   }
 }
 
-
 data "template_file" "userdata_agent" {
-  template = "${file("files/userdata_agent")}"
+  template = file("files/userdata_agent")
 
-  vars {
-    admin_password       = "${var.admin_password}"
-    cluster_name         = "${var.cluster_name}"
-    docker_version_agent = "${var.docker_version_agent}"
-    rancher_version      = "${var.rancher_version}"
-    server_address       = "${aws_instance.rancherserver.public_ip}"
+  vars = {
+    admin_password       = var.admin_password
+    cluster_name         = var.cluster_name
+    docker_version_agent = var.docker_version_agent
+    rancher_version      = var.rancher_version
+    server_address       = aws_instance.rancherserver.public_ip
   }
 }
 
 output "rancher-url" {
   value = ["https://${aws_instance.rancherserver.public_ip}"]
 }
+
