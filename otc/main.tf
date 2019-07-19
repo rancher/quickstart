368c28e1ae31713504b0706f255e5ac871ba3d04
Frank Kloeker <f.kloeker@telekom.de>
2019-07-20 00:00:12 +0200

    boot from bigger volumes and add more nodes

--- otc/main.tf
+++ otc/main.tf
@@ -39,67 +39,149 @@ resource "opentelekomcloud_compute_keypair_v2" "quickstart-key" {
   public_key = "${var.public_key}"
 }
 
-resource "opentelekomcloud_compute_instance_v2" "ranchermaster_1" {
-  name            = "ranchermaster_1"
-  image_id        = "${data.opentelekomcloud_images_image_v2.image.id}"
-  flavor_id       = "${var.flavor_id}"
-  key_pair        = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
-  security_groups = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
-  user_data       = "${data.template_file.userdata_server.rendered}"
+resource "opentelekomcloud_compute_instance_v2" "ranchermaster" {
+  name              = "ranchermaster"
+  availability_zone = "${var.availability_zone1}"
+  flavor_id         = "${var.flavor_id}"
+  key_pair          = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
+  security_groups   = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
+  user_data         = "${data.template_file.userdata_server.rendered}"
   network {
     uuid = "${opentelekomcloud_vpc_subnet_v1.subnet.id}"
   }
+  block_device {
+    boot_index            = 0
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "${data.opentelekomcloud_images_image_v2.image.id}"
+    delete_on_termination = true
+    volume_size           = 30
+  }
 }
 
-resource "opentelekomcloud_networking_floatingip_v2" "eip_1" {
+resource "opentelekomcloud_networking_floatingip_v2" "eip_ranchermaster" {
   pool = "admin_external_net"
 }
 
-resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_1" {
-  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_1.address}"
-  instance_id = "${opentelekomcloud_compute_instance_v2.ranchermaster_1.id}"
+resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_ranchermaster" {
+  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_ranchermaster.address}"
+  instance_id = "${opentelekomcloud_compute_instance_v2.ranchermaster.id}"
 }
 
 resource "opentelekomcloud_compute_instance_v2" "rancheragent_1-all" {
-  name            = "rancheragent_1-all"
-  image_id        = "${data.opentelekomcloud_images_image_v2.image.id}"
-  flavor_id       = "${var.flavor_id}"
-  key_pair        = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
-  security_groups = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
-  user_data       = "${data.template_file.userdata_agent.rendered}"
+  name              = "rancheragent_1-all"
+  availability_zone = "${var.availability_zone1}"
+  flavor_id         = "${var.flavor_id}"
+  key_pair          = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
+  security_groups   = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
+  user_data         = "${data.template_file.userdata_agent.rendered}"
   network {
     uuid = "${opentelekomcloud_vpc_subnet_v1.subnet.id}"
   }
+  block_device {
+    boot_index            = 0
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "${data.opentelekomcloud_images_image_v2.image.id}"
+    delete_on_termination = true
+    volume_size           = 30
+  }
 }
 
-resource "opentelekomcloud_networking_floatingip_v2" "eip_2" {
+resource "opentelekomcloud_networking_floatingip_v2" "eip_rancheragent_1" {
   pool = "admin_external_net"
 }
 
-resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_2" {
-  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_2.address}"
+resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_rancheragent_1" {
+  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_rancheragent_1.address}"
   instance_id = "${opentelekomcloud_compute_instance_v2.rancheragent_1-all.id}"
 }
 
 resource "opentelekomcloud_compute_instance_v2" "rancheragent_2-all" {
-  name            = "rancheragent_2-all"
-  image_id        = "${data.opentelekomcloud_images_image_v2.image.id}"
-  flavor_id       = "${var.flavor_id}"
-  key_pair        = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
-  security_groups = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
-  user_data       = "${data.template_file.userdata_agent.rendered}"
+  name              = "rancheragent_2-all"
+  availability_zone = "${var.availability_zone2}"
+  flavor_id         = "${var.flavor_id}"
+  key_pair          = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
+  security_groups   = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
+  user_data         = "${data.template_file.userdata_agent.rendered}"
   network {
     uuid = "${opentelekomcloud_vpc_subnet_v1.subnet.id}"
   }
+  block_device {
+    boot_index            = 0
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "${data.opentelekomcloud_images_image_v2.image.id}"
+    delete_on_termination = true
+    volume_size           = 30
+  }
 }
 
-resource "opentelekomcloud_networking_floatingip_v2" "eip_3" {
+resource "opentelekomcloud_networking_floatingip_v2" "eip_rancheragent_2" {
   pool = "admin_external_net"
 }
 
-resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_3" {
-  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_3.address}"
+resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_rancheragent_2" {
+  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_rancheragent_2.address}"
   instance_id = "${opentelekomcloud_compute_instance_v2.rancheragent_2-all.id}"
+}
+
+resource "opentelekomcloud_compute_instance_v2" "rancheragent_3-all" {
+  name              = "rancheragent_2-all"
+  availability_zone = "${var.availability_zone3}"
+  flavor_id         = "${var.flavor_id}"
+  key_pair          = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
+  security_groups   = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
+  user_data         = "${data.template_file.userdata_agent.rendered}"
+  network {
+    uuid = "${opentelekomcloud_vpc_subnet_v1.subnet.id}"
+  }
+  block_device {
+    boot_index            = 0
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "${data.opentelekomcloud_images_image_v2.image.id}"
+    delete_on_termination = true
+    volume_size           = 30
+  }
+}
+
+resource "opentelekomcloud_networking_floatingip_v2" "eip_rancheragent_3" {
+  pool = "admin_external_net"
+}
+
+resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_rancheragent_3" {
+  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_rancheragent_3.address}"
+  instance_id = "${opentelekomcloud_compute_instance_v2.rancheragent_3-all.id}"
+}
+
+resource "opentelekomcloud_compute_instance_v2" "rancherworker_1-worker" {
+  name              = "rancherworker_1-worker"
+  availability_zone = "${var.availability_zone3}"
+  flavor_id         = "${var.flavor_id}"
+  key_pair          = "${opentelekomcloud_compute_keypair_v2.quickstart-key.name}"
+  security_groups   = ["${opentelekomcloud_networking_secgroup_v2.quickstart-secgroup.id}"]
+  user_data         = "${data.template_file.userdata_agent.rendered}"
+  network {
+    uuid = "${opentelekomcloud_vpc_subnet_v1.subnet.id}"
+  }
+  block_device {
+    boot_index            = 0
+    source_type           = "image"
+    destination_type      = "volume"
+    uuid                  = "${data.opentelekomcloud_images_image_v2.image.id}"
+    delete_on_termination = true
+    volume_size           = 30
+  }
+}
+
+resource "opentelekomcloud_networking_floatingip_v2" "eip_rancherworker_1" {
+  pool = "admin_external_net"
+}
+
+resource "opentelekomcloud_compute_floatingip_associate_v2" "eip_rancherworker_1" {
+  floating_ip = "${opentelekomcloud_networking_floatingip_v2.eip_rancherworker_1.address}"
+  instance_id = "${opentelekomcloud_compute_instance_v2.rancherworker_1-worker.id}"
 }
 
 data "template_file" "userdata_server" {
@@ -119,6 +201,6 @@ data "template_file" "userdata_agent" {
     cluster_name         = "${var.cluster_name}"
     docker_version_agent = "${var.docker_version_agent}"
     rancher_version      = "${var.rancher_version}"
-    server_address       = "${opentelekomcloud_compute_instance_v2.ranchermaster_1.access_ip_v4}"
+    server_address       = "${opentelekomcloud_compute_instance_v2.ranchermaster.access_ip_v4}"
   }
 }
