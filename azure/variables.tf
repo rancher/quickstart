a6b5671b9b3e399c4d597d14b5e4d47d3f1ff6e5
Matt Nikkel <matt.nikkel@rancher.com>
2020-05-19 10:46:38 -0400

    Update variables to install Rancher v2.4.3 by default

--- azure/variables.tf
+++ azure/variables.tf
@@ -70,8 +70,8 @@ variable "cert_manager_version" {
 
 variable "rancher_version" {
   type        = string
-  description = "Rancher server version (format v0.0.0)"
-  default     = "v2.3.5"
+  description = "Rancher server version (format: v0.0.0)"
+  default     = "v2.4.3"
 }
 
 # Required
