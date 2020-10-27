19abcc1d80ce533a83fe68d819fd22305e02debf
Matt Nikkel <matt.nikkel@rancher.com>
2020-10-27 13:16:23 -0400

    Bump all provider versions

--- aws/versions.tf
+++ aws/versions.tf
@@ -2,14 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.1.0"
+      version = "3.12.0"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
+      version = "2.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "2.2.0"
+      version = "3.0.0"
     }
   }
   required_version = ">= 0.13"
--- do/versions.tf
+++ do/versions.tf
@@ -2,14 +2,15 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "1.22.1"
+      version = "2.0.1"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
+      version = "2.0.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "2.2.0"
+      version = "3.0.0"
     }
   }
   required_version = ">= 0.13"
