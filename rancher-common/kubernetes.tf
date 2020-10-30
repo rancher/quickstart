ffc78170548b1f013910baa70cca7fe907ee4fa8
Murali Paluru <leodotcloud@gmail.com>
2020-10-30 19:41:11 +0530

    fix minor bug, add pathexpand

--- rancher-common/kubernetes.tf
+++ rancher-common/kubernetes.tf
@@ -1,7 +1,7 @@
 # Kubernetes resources
 
 locals {
-  cert_manager_crds_content  = file(join("/", [path.module, "files/cert-manager/crds-${var.cert_manager_version}.yaml"]))
+  cert_manager_crds_content  = var.ingress_tls_source == "secret" ? "" : file(join("/", [path.module, "files/cert-manager/crds-${var.cert_manager_version}.yaml"]))
   cert_manager_crds_sections = split("---", local.cert_manager_crds_content)
 }
 
